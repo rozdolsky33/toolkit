519334cb30c6f6ea9b8732484b072b53139c6d28
Volodymyr Rozdolsky <rozdolskyvolodymyr@gmail.com>
2024-09-27 17:13:45 -0400

    troubleshooting action go v2

--- tools_test.go
+++ tools_test.go
@@ -91,3 +91,51 @@ func TestTools_UploadFiles(t *testing.T) {
 		wg.Wait()
 	}
 }
+
+func TestTools_UploadOneFile(t *testing.T) {
+	//set up a pipe to avoid buffering
+	pr, pw := io.Pipe()
+	writer := multipart.NewWriter(pw)
+	go func() {
+		defer writer.Close()
+
+		// crete the form data field 'file'
+		part, err := writer.CreateFormFile("file", "./testdata/img.png")
+		if err != nil {
+			t.Error(err)
+		}
+
+		f, err := os.Open("./testdata/img.png")
+
+		if err != nil {
+			t.Error(err)
+		}
+		defer f.Close()
+
+		img, _, err := image.Decode(f)
+		if err != nil {
+			t.Error("error decoding image", err)
+		}
+		err = png.Encode(part, img)
+		if err != nil {
+			t.Error(err)
+		}
+	}()
+	// read from the pipe which receives data
+	request := httptest.NewRequest(http.MethodPost, "/upload", pr)
+	request.Header.Add("Content-Type", writer.FormDataContentType())
+
+	var testTools Tools
+
+	uploadedFiles, err := testTools.UploadOneFile(request, "./testdata/uploads/", true)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if _, err := os.Stat(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles.NewFileName)); os.IsNotExist(err) {
+		t.Errorf("file not uploaded %s", err.Error())
+	}
+	// Clean up
+	_ = os.Remove(fmt.Sprintf("./testdata/uploads/%s", uploadedFiles.NewFileName))
+}
